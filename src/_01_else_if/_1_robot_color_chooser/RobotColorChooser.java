6457072c97002d56ed7cf1121551ea522a96e380
root <root@2df08443ac2c>
2020-04-08 23:54:32 +0000

    High Low Game

--- src/_01_else_if/_1_robot_color_chooser/RobotColorChooser.java
+++ src/_01_else_if/_1_robot_color_chooser/RobotColorChooser.java
@@ -3,15 +3,58 @@ package _01_else_if._1_robot_color_chooser;
 
 import javax.swing.JOptionPane;
 import java.awt.Color;
+import java.util.Random;
+
+import org.jointheleague.graphical.robot.Robot;
 
 public class RobotColorChooser {
 	public static void main(String[] args) {
-		
+		for(int i =0;i<10;i++) {
 		//1. Create a new Robot
-	    //2. Make the robot draw a shape (this will take more than one line of code)		
+		Robot rob = new Robot();
+	    //2. Make the robot draw a shape (this will take more than one line of code)
+
 		//3. Set the pen width to 10
+		rob.setPenWidth(10);
+		rob.hide();
 		//4. Ask the user what color pen they would like the robot to draw with
+		String color = JOptionPane.showInputDialog("What color pen would you like the robot to draw with?");
 		//5. Use an if/else statement to set the pen color that the user requested
+		if (color.contentEquals("Red")) {
+			rob.setPenColor(255, 0, 0);
+		}
+		else if(color.contentEquals("Orange")) {
+			rob.setPenColor(255, 186, 10);
+		}
+		else if(color.contentEquals("Yellow")) {
+			rob.setPenColor(254, 255, 0);
+		}
+		else if(color.contentEquals("Green")) {
+			rob.setPenColor(0,255,18);
+		}
+		else if(color.contentEquals("Blue")) {
+			rob.setPenColor(0, 70, 255);
+		}
+		else if(color.contentEquals("Purple")) {
+			rob.setPenColor(178, 0, 255);
+		}
+		else if(color.contentEquals("Black")) {
+			rob.setPenColor(0,0,0);
+		}
+		else {
+			rob.setRandomPenColor();
+		}
+		rob.setSpeed(10);
+		rob.penDown();
+		rob.move(50);
+		rob.turn(90);
+		rob.move(50);
+		rob.turn(90);
+		rob.move(50);
+		rob.turn(90);
+		rob.move(50);
+		rob.turn(90);
+		}
         //6. If the user doesn’t enter anything, choose a random color
         //7. Put a loop around your code so that you keep asking the user for more colors & drawing them
 		
--- src/_01_else_if/_2_crazy_cat_lady/CrazyCatLady.java
+++ src/_01_else_if/_2_crazy_cat_lady/CrazyCatLady.java
@@ -3,31 +3,50 @@ package _01_else_if._2_crazy_cat_lady;
 
 import java.net.URI;
 
+import javax.swing.JOptionPane;
+
 public class CrazyCatLady {
 	public static void main(String[] args) {
 		
 		// 1. Ask the user how many cats they have
+		String cats = JOptionPane.showInputDialog("How many Cats do you have?");
 		// 2. Convert their answer into an int
+		int number = Integer.parseInt(cats);
 		// 3. If they have 3 or more cats, tell them they are a crazy cat lady
+		if (number >= 3) {
+			JOptionPane.showMessageDialog(null, "You're a crazy cat lady.");
+		}
 		// 4. If they have less than 3 cats AND more than 0 cats, call the method below to show them a cat video
 		//    Hint: You can use the && operator 
-		// 5. If they have 0 cats, show them a video of A Frog Sitting on a Bench Like a Human
-		
-		
-		
-		
-		
-		
-	}
-
-	static void playVideo(String videoURL) {
-		try {
-			URI uri = new URI(videoURL);
-			java.awt.Desktop.getDesktop().browse(uri);
-		} catch (Exception e) {
-			e.printStackTrace();
+		else if (number<3 && number >0) {
+			playVideo("https://www.youtube.com/watch?v=XyNlqQId-nk");
 		}
+		// 5. If they have 0 cats, show them a video of A Frog Sitting on a Bench Like a Human
+		else {
+			playVideo("https://www.youtube.com/watch?v=oj_yLBltPE8");
+		}
+		
+		
+		
+		
+		
 	}
 
+	public static void playVideo(String videoID) {
+		// Workaround for Linux because "Desktop.getDesktop().browse()" doesn't work on
+		// some Linux implementations
+        try{
+		if (System.getProperty("os.name").toLowerCase().contains("linux")) {
+				if (Runtime.getRuntime().exec(new String[] { "which", "xdg-open" }).getInputStream().read() != -1) {
+					Runtime.getRuntime().exec(new String[] { "xdg-open", videoID });
+				}
+		} else {
+				URI uri = new URI(videoID);
+				java.awt.Desktop.getDesktop().browse(uri);
+		}	
+            } catch (Exception e) {
+				e.printStackTrace();
+			}
+	}
 }
 
